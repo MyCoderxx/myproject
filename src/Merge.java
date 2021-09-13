1bcce88aa74633ad27e12233cefd39fb6a21f1e7
3119005072 <2810524364@qq.com>
2021-09-13 22:22:05 +0800

    first commit

---
+++ src/Merge.java
@@ -0,0 +1,30 @@
+public class Merge {
+    public void sort(Comparable[] a) {
+
+
+    }
+
+    public void sort(Comparable a[], int low, int height) {
+
+    }
+
+    public void merge(Comparable[]a,int low,int mid,int height){
+
+  }
+
+
+
+
+
+    public static boolean compare(Comparable a,Comparable b){
+        return a.compareTo(b)<0;
+    }
+
+    public static void  exchange(Comparable []a,int i,int j){
+        Comparable temp;
+        temp=a[i];
+        a[i]=a[j];
+        a[j]=temp;
+    }
+
+}
