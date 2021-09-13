b778dd261da8032caae05a076812d5a7dc8c4a0d
3119005072 <2810524364@qq.com>
2021-09-14 00:06:06 +0800

    未完成版

--- src/Merge.java
+++ src/Merge.java
@@ -1,17 +1,56 @@
 public class Merge {
-    public void sort(Comparable[] a) {
+    //对数组a进行排序
+    private static Comparable[] assist;
 
+    public static void sort(Comparable[] a) {
+        assist=new Comparable[a.length];
+        int low=0;
+        int hi=a.length-1;
+        //对整个数组进行排序
+        sort(a,low,hi);
 
     }
 
-    public void sort(Comparable a[], int low, int height) {
-
+    //对数组a的从索引low到索引height元素进行排序
+    public static void sort(Comparable a[], int low, int height) {
+         if(low==height){//分组完成
+             return;
+         }
+         //分组界限
+         int mid=low+(height-low)/2;
+         //递归对左子组进行分组
+        sort(a,low,mid);
+        //递归对右子组进行分组
+        sort(a,mid+1,height);
+        //对各组进行归并排序
+        merge(a,low,mid,height);
     }
 
-    public void merge(Comparable[]a,int low,int mid,int height){
+    //进行归并排序
+    public static void merge(Comparable[]a,int low,int mid,int height){
+     int p=mid+1;//辅助指针
+     int q=low;//辅助指针
+     int help=low;//辅助数组的辅助指针
+        while (q!=mid+1&&p!=height+1){//左子组辅助指针不超过mid，右子组不超过height
+            if(compare(a[q],a[p])){//如果左边元素比右边元素小
+                assist[help++]=a[q++];
+            }else {//如果右边元素比左边元素小
+                assist[help++]=a[p++];
+            }
+        }
 
-  }
+        while(q!=mid+1){//将剩余元素加到辅助数组中
+            assist[help++]=a[q++];
+        }
 
+        while(p!=height+1){//将剩余元素加到辅助数组中
+            assist[help++]=a[p++];
+        }
+         //将辅助数组复制到原数组中
+        for (int i = 0; i < a.length; i++) {
+            a[i]=assist[i];
+        }
+    }
 
 
 
---
+++ src/Test.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        Integer[]a={1,9,8,3,4,6,7};
+        Merge.sort(a);
+        System.out.println(Arrays.toString(a));
+    }
+}
