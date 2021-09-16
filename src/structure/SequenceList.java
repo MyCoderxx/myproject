1b2c10c56ed2f690ee75ef1b33ab7c163abd8149
3119005072 <2810524364@qq.com>
2021-09-17 00:02:11 +0800

    增加顺序表实现

--- src/structure/SequenceList.java
+++ src/structure/SequenceList.java
@@ -8,20 +8,26 @@ public class SequenceList<T> implements Iterable  {
 
     public SequenceList(int capacity){//capacity为初始容量
         this.elems=(T[]) new Object[capacity];
+        N=0;
     }
 
     //将顺序表长度置为newSize大小
     public void resize(int newSize){
-
+        T[]temp=elems;
+        elems= (T[]) new Object[newSize];
+        for (int i = 0; i < temp.length; i++) {
+            elems[i]=temp[i];
+        }
     }
 
     //将表清空
     public void clear(){
+        this.N=0;
     }
 
     //判断是否为空
     public boolean isempty(){
-        return false;
+        return this.N==0;
     }
 
     //返回数组元素个数
@@ -31,27 +37,50 @@ public class SequenceList<T> implements Iterable  {
 
    //得到i索引处元素值
    public T get(int i){
-        return null;
+        return elems[i];
    }
 
    //往数组末尾插入元素
     public void insert(T elem){
-
+        if(N==elems.length){//顺序表已满
+            resize(elems.length*2);//容量增加2倍
+        }
+      elems[N++]=elem;
     }
 
     //在指定索引处插入元素
     public void insert(int i,T elem){
-
+        if(N==elems.length){//顺序表已满
+            resize(elems.length*2);//容量增加2倍
+        }
+        for (int j = N; j>i; j--) {//i索引处后的元素向后移动
+            elems[j]=elems[j-1];
+        }
+        elems[i]=elem;
+        N++;
     }
 
     //删除i索引处值并返回
     public T remove(int i){
-      return null;
+        T elem=elems[i];
+        for (int j = i; j<N-1; j++) {//i索引处后的元素向后移动
+            elems[j]=elems[j+1];
+        }
+        N--;
+        if(N<elems.length/4){//顺序表太空
+            resize(elems.length/2);//容量减少2倍
+        }
+        return elem;
     }
 
     //查找元素第一次出现的位置
     public int indexOf(T elem){
-        return 0;
+        for (int i = 0; i < N; i++) {
+            if(elems[i].equals(elem)){
+                return i;
+            }
+        }
+        return -1;
     }
 
     //获得迭代器
@@ -69,12 +98,12 @@ public class SequenceList<T> implements Iterable  {
 
         @Override
         public boolean hasNext() {
-            return false;
+            return cusor<N;
         }
 
         @Override
         public Object next() {
-            return null;
+            return elems[cusor++];
         }
     }
 }
